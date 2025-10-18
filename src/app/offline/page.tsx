ffe198c1a3085ff75347ae26f71c257f23c84680
Hamid Yeganeh <hamidyeganeh82@gmail.com>
2025-10-18 14:39:24 +0330

    [add] cahce

---
+++ src/app/offline/page.tsx
@@ -0,0 +1,7 @@
+export default function OfflinePage() {
+  return (
+    <div className="h-dvh bg-red-500 flex items-center justify-center">
+      <h1>You are offfffffline</h1>
+    </div>
+  );
+}
